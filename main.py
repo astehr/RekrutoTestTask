b8f50b20a17def35a8a957cb13a81c7c254cfa58
astehr <75850076+astehr@users.noreply.github.com>
2022-12-05 00:34:46 +0400

    Fix None value

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
+
+
 @app.route('/', methods=['GET'])
 def greeting():
     args = request.args
     name = args.get('name')
     message = args.get('message')
-    final_message = f"Hello {name}! {message}!"
-    return render_template('index.html', final_message=final_message)
+    if name is None or message is None:
+        return render_template('index.html', final_message="Please specify both name and message!")
+
+    return render_template('index.html', final_message=f"Hello {name}! {message}!")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
